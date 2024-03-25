e033d68eb9800c720f55af8376350cf39c04e93b
Johannes Pessi <pessijoh@arcada.fi>
2024-03-25 15:28:36 +0200

    code challenge 25.03

--- app/src/main/java/fi/arcada/codechallenge/MainActivity.java
+++ app/src/main/java/fi/arcada/codechallenge/MainActivity.java
@@ -1,19 +1,52 @@
 package fi.arcada.codechallenge;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView outputText;
+    EditText inputText;
+
+    TextView valueText;
+
+    double[] values = {1, 3, 5, 6, 7, 8, 8, 8, 7, 14};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        outputText = findViewById(R.id.outText);
+        inputText = findViewById(R.id.inputText);
+        valueText = findViewById(R.id.outValue);
+
+
+        outputText.setText("Min app funkar!");
+
+        double average = calculateAverage(values);
     }
-}
+
+        public static double calculateAverage(double[] array){
+        double sum = 0;
+        for (double num : array){
+            sum += num;
+        }
+        double average = sum / array.length;
+        return average;
+    }
+
+    public void buttonHandler(View view) {
+        outputText.setText(inputText.getText().toString());
+
+    }
+    public void buttonCalc (View view){
+        double average = calculateAverage(values);
+        valueText.setText("average. " +average);
+
+        }
+    }
