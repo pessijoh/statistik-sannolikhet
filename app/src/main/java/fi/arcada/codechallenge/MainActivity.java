5b545ef1e7ef9c87d2267bde8d9963c15f289e37
Johannes Pessi <pessijoh@arcada.fi>
2024-03-28 14:51:44 +0200

    28.3

--- app/src/main/java/fi/arcada/codechallenge/MainActivity.java
+++ app/src/main/java/fi/arcada/codechallenge/MainActivity.java
@@ -4,8 +4,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import java.util.Arrays;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     TextView valueText;
 
-    double[] values = {1, 3, 5, 6, 7, 9, 8, 8, 7, 14};
+    double[] values = {658457, 297132, 244223, 239206, 209551, 195137, 144473, 121543, 120027, 83482, 80454, 77261, 72634, 67971, 67615, 64736, 64180, 52122, 51400, 51241, 51149, 47909, 46880, 45988, 45226, 38959, 37232, 36493, 35497, 34884, 33533, 32622, 32547, 28521, 27484, 25655, 24810, 24260, 23998, 21333, 20958, 20695, 20197, 19982, 19973, 19767, 19702, 19579, 19097, 18344, 18318, 17253, 16573, 16467, 15808, 15628, 15463, 15357, 15312, 15165, 15086, 14643, 14203, 12890, 12669, 12662, 12412, 11742, 11197, 11041, 10543, 10396, 10396, 9877, 9870, 9563, 9562, 9443, 9423, 9311, 9117, 8978, 8717, 8563, 8456, 7979, 7928, 7759, 7702, 7497, 7105, 7102, 6951, 6891, 6877, 6802, 6785, 6613, 6506, 6465, 6380, 6286, 6070, 5484, 5390, 4964, 1289};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,25 +32,34 @@ public class MainActivity extends AppCompatActivity {
 
         outputText.setText("Min app funkar!");
 
-        double average = calculateAverage(values);
+
+        }
+
+    public static double calcMedian(double[] array) {
+        Arrays.sort(array);
+        int midIndex = array.length / 2;
+        if (array.length % 2 == 1) {
+            return array[midIndex];
+        } else {
+            return (array[midIndex - 1] + array[midIndex]) / 2.0;
+        }
     }
 
-        public static double calculateAverage(double[] array){
+    public static double calculateAverage(double[] array) {
         double sum = 0;
-        for (double num : array){
+        for (double num : array) {
             sum += num;
         }
-        double average = sum / array.length;
-        return average;
+        return sum / array.length;
     }
 
     public void buttonHandler(View view) {
         outputText.setText(inputText.getText().toString());
-
     }
-    public void buttonCalc (View view){
+
+    public void buttonCalc(View view) {
         double average = calculateAverage(values);
-        valueText.setText("average. " +average);
-
-        }
+        double median = calcMedian(values);
+        valueText.setText("Average: " + average + "\nMedian: " + median);
     }
+}
